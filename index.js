7419c253fdd5b3c02225a4cf40c753d1bc9e502d
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-06-01 16:25:13 +0600

    this user conteol router and user manage router compalte

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+//Student_tutorsDB
 const uri = `mongodb+srv://${process.env.DATABASEUSER}:${process.env.DATABASEPASS}@datafind.xfgov3s.mongodb.net/?retryWrites=true&w=majority&appName=datafind`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,17 +28,60 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
+    // user collection
+    const usercollection = client.db("Student_tutorsDB").collection("alluser");
+
+    // create jwt
     app.post("/jwtCreate", (req, res)=>{
         const email = req.body;
         const token = jwt.sign(email, process.env.TOKEN_SECRATE, {expiresIn : "1hr"})
         res.send({token})
     })
 
-    // payment router
+    // user authorization check
+    app.post("/verifyuser")
+    // Test router
     app.get("/", (req, res) => {
       res.send("Hello World!");
     });
 
+
+    //user admin student role check
+    app.get("/checkRole/:email", async(req, res)=>{
+        const email = req.params.email;
+        if(!email){
+            return res.send({message: "Unauthorize Use"})
+        }
+        if(email){
+            const roles = await usercollection.findOne({email});
+            if(roles){
+                if(roles.role === "admin"){
+                    return res.send({roles : "admin"})
+                }
+                if(roles.role === "tutor"){
+                    return res.send({roles : "tutor"})
+                }
+                if(roles.role === "student"){
+                    return res.send({roles : "student"})
+                }
+            }
+        }
+    })
+
+    //set user role
+    app.post("/user-role-set", async(req, res)=>{
+        const user = req.body;
+
+        const existUser = await usercollection.findOne({email : user.email})
+        if(existUser){
+            return res.send({message : "This User Already Exist"})
+        }else{
+            const result = await usercollection.insertOne(user);
+            res.send(result)
+        }
+    })
+
+    // payment Router
     app.post("/create-payment-intent", async (req, res) => {
       const price = req.body;
       const money = parseInt(price.items * 100);
